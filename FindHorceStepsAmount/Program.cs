79462d451681f792f313edd9df3e10085a151263
Andrii <andriy0liulka@gmail.com>
2024-07-28 12:08:58 +0300

    Resolved FindHourseStepsAmount

--- FindHorceStepsAmount/Program.cs
+++ FindHorceStepsAmount/Program.cs
@@ -1,24 +1,36 @@
-﻿// See https://aka.ms/new-c//https://https://www.youtube.com/watch?v=tgWTBwjy--A&ab_channel=%D0%92%D0%BE%D0%B8%D0%BD%D1%81%D1%82%D0%B2%D1%83%D1%8E%D1%89%D0%B8%D0%B5%D0%90%D0%BC%D0%B5%D1%82%D0%B8%D1%81%D1%82%D1%8B
+﻿// See https://www.youtube.com/watch?v=tgWTBwjy--A&ab_channel=%D0%A1%D0%B0%D1%88%D0%B0%D0%9B%D1%83%D0%BA%D0%B8%D0%BD
 
+using System.Diagnostics.CodeAnalysis;
 
 int FindDistance(int target_x, int target_y)
 {
     int layerNumber = 0;
-    var currentLayer = new int[] { target_x, target_y };
-    while (target_x != currentLayer[0] && target_y != currentLayer[1])
+    var currentPosition = new int[] { 0, 0 };
+
+    HashSet<int[]> previosLayer = new HashSet<int[]>(new Comparer());
+    HashSet<int[]> currentLayer = new HashSet<int[]>(new Comparer());
+    currentLayer.Add(currentPosition);
+
+    while (!currentLayer.Contains([ target_x, target_y ]))
     {
-        var currentX = currentLayer[0];
-        var currentY = currentLayer[1];
-
-        var allRoundLayers = NextLayers(currentX, currentY);
-
-        for ( var i = 0; i < allRoundLayers.Length; i++ )
+        var nextLayer = new HashSet<int[]>(new Comparer());
+        foreach (var layerRepresenter in currentLayer)
         {
-
+            foreach (var nextCells in NextLayers(layerRepresenter[0], layerRepresenter[1]))
+            {
+                if (!previosLayer.Contains(nextCells))
+                {
+                    nextLayer.Add(nextCells);
+                }
+            }
         }
-
+        currentLayer = nextLayer;
+        previosLayer = currentLayer;
         layerNumber++;
     }
+
+
+    return layerNumber;
 }
 
 int[][] NextLayers(int current_x, int current_y)
@@ -30,4 +42,21 @@ int[][] NextLayers(int current_x, int current_y)
         [current_x-1, current_y-2],[current_x-2, current_y-1],
         [current_x-2, current_y+1],[current_x+2, current_y-1],
     ];
+}
+
+var result = FindDistance(5,2);
+Console.WriteLine(result);
+
+
+class Comparer : IEqualityComparer<int[]>
+{
+    public bool Equals(int[]? x, int[]? y)
+    {
+        return GetHashCode(x) == GetHashCode(y);
+    }
+
+    public int GetHashCode([DisallowNull] int[] obj)
+    {
+        return obj[0].GetHashCode() + obj[1].GetHashCode();
+    }
 }
