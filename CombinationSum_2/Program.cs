a584b9fe2365a57a084f1f4f42fa850635c8ab46
AndriiLiulka <andreyglasson@gmail.com>
2025-02-05 00:54:50 +0200

    Made a draft of new solution.

---
+++ CombinationSum_2/Program.cs
@@ -0,0 +1,43 @@
+﻿//https://leetcode.com/problems/combination-sum-ii/
+
+namespace CombinationSum_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             var result = new Solution().CombinationSum2([], 0);
+
+        }
+
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+        {
+            var possibleNumbers = candidates.Where(x => x <= target);
+            var dictOccurrences = new Dictionary<int, int>();
+
+            foreach (var possibleNumber in possibleNumbers)
+            {
+                var isPresent = dictOccurrences.ContainsKey(possibleNumber);
+
+                if (isPresent)
+                {
+                    dictOccurrences[possibleNumber] += 1;
+                }
+                else
+                {
+                    dictOccurrences[possibleNumber] = 1;
+                }
+            }
+
+
+
+
+
+            return new List<IList<int>>();
+        }
+    }
+}
