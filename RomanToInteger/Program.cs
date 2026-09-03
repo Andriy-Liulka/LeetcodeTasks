7828efb1b69723a2d8f795acc2b131c3faeb29e6
AndriiLiulka <andreyglasson@gmail.com>
2026-09-03 23:43:24 +0300

    Added solution RomanToInteger.

---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,43 @@
+﻿//https://leetcode.com/problems/roman-to-integer/description
+
+namespace RomanToInteger;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var solution = new Solution();
+        Console.WriteLine(solution.RomanToInt("IV"));
+        Console.WriteLine(solution.RomanToInt("MCMXCIV"));
+        Console.WriteLine(solution.RomanToInt("XXI"));
+    }
+}
+
+public class Solution {
+    private readonly Dictionary<char, int> ValuesDictionary = new Dictionary<char, int>
+    {
+        ['I'] = 1,
+        ['V'] = 5,
+        ['X'] = 10,
+        ['L'] = 50,
+        ['C'] = 100,
+        ['D'] = 500,
+        ['M'] = 1000,
+    };
+    public int RomanToInt(string s)
+    {
+        int sum = 0;
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(i + 1 <= s.Length - 1){
+                if(ValuesDictionary[s[i]] < ValuesDictionary[s[i + 1]])
+                    sum -= ValuesDictionary[s[i]];
+                else
+                    sum += ValuesDictionary[s[i]];
+            }
+            else
+                sum += ValuesDictionary[s[i]];
+        }
+        return sum;
+    }
+}
