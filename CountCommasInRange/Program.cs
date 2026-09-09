b3829861d5999de29d174bd2a31a79dc2bb3b965
AndriiLiulka <andreyglasson@gmail.com>
2026-09-09 21:30:39 +0300

    Added resolution for 2 more tasks.

---
+++ CountCommasInRange/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CountCommasInRange;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var solution = new Solution();
+        //Console.WriteLine(solution.CountCommas(11280));
+        Console.WriteLine(solution.CountCommas(100_000));
+    }
+}
+
+public class Solution {
+    public int CountCommas(int n)
+    {
+        // if (n < 1000)
+        // {
+        //     return 0;
+        // }
+        //
+        // int numbersInDigit = (int)Math.Log10(n);
+        //
+        // int numberOf3ths = (int)Math.Round((decimal)numbersInDigit/3);
+        //
+        // var result = (int)(n - (Math.Pow(10, numbersInDigit))) + 1;
+        //
+        // return result * numberOf3ths;
+
+        if (n < 1000)
+            return 0;
+
+        // int numberoOfCommas = 0;
+        // for (int i = 1; i <= n; i++)
+        // {
+        //     int numberInDigit = (int)Math.Log10(i);
+        //     int numberOfThirds = numberInDigit/3;
+        //     numberoOfCommas += numberOfThirds;
+        // }
+
+        return n - 999;
+    }
+}
---
+++ MinimumDistanceToTheTargetElement/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MinimumDistanceToTheTargetElement;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var solution = new Solution();
+        Console.WriteLine(solution.GetMinDistance([5,3,6], 5 ,2));
+        Console.WriteLine(solution.GetMinDistance([1,2,3,4,5], 3 ,4));
+    }
+}
+
+public class Solution {
+    public int GetMinDistance(int[] nums, int target, int start)
+    {
+        int i = start;
+        int j = start;
+        while (i >= 0 || j < nums.Length)
+        {
+            if (i >= 0 && nums[i] == target)
+                return Math.Abs(i - start);
+            if (j < nums.Length && nums[j] == target)
+                return Math.Abs(j - start);
+            i--;
+            j++;
+        }
+        return 0;
+    }
+}
